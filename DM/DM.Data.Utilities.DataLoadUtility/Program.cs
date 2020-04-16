4c319d303fefea57725df108856a931c77023ffc
Phillip Copley <pcopley@qita.com>
2020-04-15 21:09:08 -0400

    Console utility to load data from http://www.dnd5eapi.co/

---
+++ DM/DM.Data.Utilities.DataLoadUtility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DM.Data.Utilities.DataLoadUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
